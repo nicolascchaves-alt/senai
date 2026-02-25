4b801f35644d653594d51d5c70216c898895c417
nicolascchaves-alt <nicolas_c_chaves@estudante.sesisenai.org.br>
2026-02-25 20:17:03 +0000

    update

---
+++ atividade6.py
@@ -0,0 +1,9 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade:"))
+while idade > 120 or idade < 0:
+    idade = int(input)("idade(anos completos - ate 120)")
+dias_vida = idade * 365
+print(f"{nome}, voce viveu {dias de vida}")
+
+    
+
