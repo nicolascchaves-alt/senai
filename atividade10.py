0200a448efb82c7e098a21498c08f83651e83bd9
nicolascchaves-alt <nicolas_c_chaves@estudante.sesisenai.org.br>
2026-03-11 19:22:13 +0000

    update

--- atividade10.py
+++ atividade10.py
@@ -1,11 +1,25 @@
-medicoes
- 
- print("digite 8 medicoes de corrente elétrica (em amperes):")
-for i in range(8):
-    corrente = float(input(f"Medicao {i+1}: "))
-    medicoes.append(corrente)
+soma = 0 
+acima_15 = 0
+acima_20 = 0
+sobrecarga = False
+alerta = False
+acima_200 = 0
+for cont in range(8):
+    amper = float(input(f"digite o {cont + 1} amper: "))
+    soma += amper
+    if amper > 15:
+        acima_15 += 1
+    if amper > 20:
+        sobrecarga = True
+        acima_20 += 1
+        if amper + 200:
+            alerta = True
+            acima_200 += 1 
 
-acima_15a = sum(1 for m in medicoes if m > 15)
-sobrecarga = any(m > 20 for m in medicoes)
-
-media_corrente = sum(medicoes) / len(medicoes)
+media = soma / 8
+print(f"Amper media: {media}") 
+print(f"quantidade de amperes acima de 15: {acima_15} vezes")
+if sobrecarga:
+    print(" houve sobrecarga no motor {acima_20} vezes")
+if alerta:
+    print("alerta: ultrapassou 200 {acima_200} vezes")
